daa88034ec3f304ff51c568573b2768fddccee84
Kaito <e.weissenrieder94@gmail.com>
2026-04-03 11:44:40 +0200

    * update Patchnotes

--- frontend/src/components/PatchnotesLink.tsx
+++ frontend/src/components/PatchnotesLink.tsx
@@ -4,11 +4,11 @@ export default function PatchnotesLink() {
     return (
         <Wrapper>
             <a
-                href={"https://github.com/WE-Kaito/digimon-tcg-simulator/wiki/Patchnotes#30012026"}
+                href={"https://github.com/WE-Kaito/digimon-tcg-simulator/wiki/Patchnotes#03042026"}
                 target="_blank"
                 rel="noopener noreferrer"
             >
-                Patch notes (30.01.2026)
+                Patch notes (03.04.2026)
             </a>
         </Wrapper>
     );
